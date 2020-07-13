f4c4447785edd5860f672b59e54eecdafd160a60
Bing5154 <bli7@stuy.edu>
2020-07-13 10:23:11 -0400

    current code on nlt, will finish category 1 (expression) soon

---
+++ nlt.js
@@ -0,0 +1,170 @@
+'use strict';
+
+const start = (say, sendButton) => {
+	// use list to store conversation, easier to tell the difference
+	say(['Hello, welcome to Nemoji Language Center ! 😊 ',
+	'For your information as starting, our learning sections will be seperated ' +
+	'into the vocabulary section and a short quiz to test what you learned. 🈶 📝  ',
+	'Nemoji will teach you the most common words in six of the world\'s' +
+	' most popular languages using emojis!']).then(() => {
+		 sendButton('Are you ready for Nemoji? ▶️❓ ', [{title: 'Yes', payload: '0'}, 'No']);
+		});
+};
+
+const state = (payload, say, sendButton) => {
+	// help user navigate back
+	function backtrack(category, language) {
+		sendButton('What\'s next?', [{title: 'Continue learning vocabulary', payload: 'a'},
+		{title: 'Return to Category', payload: category},
+		{title: 'Learn the Word in Another 🗣️', payload: language},
+		{title: 'Return to menu', payload: '0'}, {title: 'Exit', payload: 'no'}]);
+	}
+
+    // choose activity: vocabulary or quiz?
+	if (payload === '0') {
+		say('First, please choose the learning section that you most preferred.').then( () => {
+			// message
+			sendButton('Learn Vocabulary or Take a Quiz?',
+			// buttons
+			[{title: 'Vocabulary 📖', payload: 'a'}, {title: 'Quiz 🖊️', payload: 'qz'}]);
+		 });
+		}
+
+		// choose vocabulary category here
+		if (payload === 'a') {
+			sendButton('Here is the Vocabulary section. What category would you like to learn?',
+			[{title: 'Expression 😀', payload: 'a1'}, {title: 'Animal 🐵', payload: 'a2'},
+			{title: 'Food 🍑', payload: 'a3'}, {title: 'Transportation 🚘', payload: 'a4'},
+			{title: 'Location ⛰️', payload: 'a5'}]
+			);
+		}
+		
+		
+		
+		//***********************************************************************************/
+		// ************************* Category 1: Expression (a1) ******************************//
+		//*********************************************************************************/
+		// all expressions payload starts with b
+		if (payload === 'a1') {
+			sendButton('What expression would you like to learn?',
+			[{title: 'A. 🙄', payload: 'b1'}, {title: 'B. 😀', payload: 'b2'},
+			{title: 'C. 😢', payload: 'b3'}, {title: 'D. 😡', payload: 'b4'},
+			{title: 'E. 😳', payload: 'b5'}, {title: 'F. 😱', payload: 'b6'}, 
+			{title: 'G. 😰', payload: 'b7'}, {title: 'H. 😞', payload: 'b8'}, 
+			{title: 'I. 😪', payload: 'b9'}, {title: 'J. 😌', payload: 'b10'}, 
+			{title: 'K. 😲', payload: 'b11'}, {title: 'L. 😍', payload: 'b12'}]
+			);
+		}
+
+		// ************************ Different Languages for Annoyed (b1)*********************//
+		if (payload === 'b1') {
+			sendButton('What language would you like to learn the expression in?',
+			[{title: 'A. 🇦🇪', payload: 'ARb1'}, {title: 'B. 🇨🇳', payload: 'CNb1'},
+			{title: 'C. 🇫🇷', payload: 'FRb1'}, {title: 'D. 🇮🇹', payload: 'ITb1'},
+			{title: 'E. 🇯🇵', payload: 'JPb1'}, {title: 'F. 🇪🇸', payload: 'ESb1'}]
+			);
+		}
+		// Arabic for annoyed
+		if(payload === 'ARb1') {
+			say(["🙄", "Annoyed", "منزعج", {attachment: 'audio',url: "https://bit.ly/2VLaYgn"}]).then(()=>{
+				backtrack('a1')
+			});
+		}
+		// Chinese for annoyed
+		if(payload === 'CNb1') {
+			say(["🙄", "Annoyed", "烦人", {attachment: 'audio',url: "https://bit.ly/2ZwbLDc"}]).then(()=>{
+				backtrack('a1', 'b1')
+			});
+		}
+		// French for annoyed
+		if(payload === 'FRb1') {
+			say(["🙄", "Annoyed", "Agacé", {attachment: 'audio',url: "https://bit.ly/2O6F1uJ"}]).then(()=>{
+				backtrack('a1', 'b1')
+			});
+		}
+		// Italian for annoyed
+		if(payload === 'ITb1') {
+			say(["🙄", "Annoyed", 'Infastidito/a', {attachment: 'audio',url: "https://bit.ly/3f2LBOL"}]).then(()=>{
+				backtrack('a1', 'b1')
+			});
+		}
+		// Japanese for annoyed
+		if(payload === 'JPb1') {
+			say(["🙄", "Annoyed", 'イライラする', {attachment: 'audio',url: "https://bit.ly/38tk3jn"}]).then(()=>{
+				backtrack('a1', 'b1')
+			});
+		}
+		// Spanish for annoyed
+		if(payload === 'ESb1') {
+			say(["🙄", "Annoyed", 'Molesto/a', {attachment: 'audio',url: "https://bit.ly/3iBwm1t"}]).then(()=>{
+				backtrack('a1', 'b1')
+			});
+		}
+
+		// ************************ Different Languages for happy (b2)**************************************//
+		if (payload === 'b2') {
+			sendButton('What language would you like to learn the expression in?',
+			[{title: 'A. 🇦🇪', payload: 'ARb2'}, {title: 'B. 🇨🇳', payload: 'CNb2'},
+			{title: 'C. 🇫🇷', payload: 'FRb2'}, {title: 'D. 🇮🇹', payload: 'ITb2'},
+			{title: 'E. 🇯🇵', payload: 'JPb2'}, {title: 'F. 🇪🇸', payload: 'ESb2'}]
+			);
+		}
+		// Arabic for happy
+		if(payload === 'ARb2') {
+			say(["😀", "Happy", "سعيدة", {attachment: 'audio',url: "https://bit.ly/2Z2hfGU"}]).then(()=>{
+				backtrack('a1', 'b2')
+			});
+		}
+		// Chinese for happy
+		if(payload === 'CNb2') {
+			say(["😀", "Happy", "高兴", {attachment: 'audio',url: "https://bit.ly/2AqulnD"}]).then(()=>{
+				backtrack('a1', 'b2')
+			});
+		}
+		// French for happy
+		if(payload === 'FRb2') {
+			say(["😀", "Happy", "Content", {attachment: 'audio',url: "https://bit.ly/2Z8G2sV"}]).then(()=>{
+				backtrack('a1', 'b2')
+			});
+		}
+		// Italian for happy
+		if(payload === 'ITb2') {
+			say(["😀", "Happy", 'Felice', {attachment: 'audio',url: "https://bit.ly/38vH44U"}]).then(()=>{
+				backtrack('a1', 'b2')
+			});
+		}
+		// Japanese for happy
+		if(payload === 'JPb2') {
+			say(["😀", "Happy", 'ハッピー', {attachment: 'audio',url: "https://bit.ly/2AAE7Uo"}]).then(()=>{
+				backtrack('a1', 'b2')
+			});
+		}
+		// Spanish for happy
+		if(payload === 'ESb2') {
+			say(["😀", "Happy", 'Contento', {attachment: 'audio',url: "https://bit.ly/31Inz86"}]).then(()=>{
+				backtrack('a1', 'b2')
+			});
+		}
+
+
+
+
+
+
+} // final bracket for state
+
+
+
+
+module.exports = {
+	filename: 'helloworld',
+	title: 'Nemoji Language Tutor',
+	introduction: [
+		'Welcome to Nemoji Language Center!! 🎉  \n\nHello, I am your Nemoji Tutor.' +
+		'👋🏻 🤖\nFor your information, this is an application for learning foreign ' +
+		 'language using cute emojis. 🔤 🌏 \n If you are interested to learn ' +
+		 'many languages, here is the best place to start! 🏮📚'
+	],
+	start: start,
+	state: state
+};
